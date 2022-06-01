d7ddeda50e00d67e5cfc87d35d97a29800916514
Airagon <aldo7liverpool@gmail.com>
2022-06-01 11:57:55 +0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'vikelaia-qr-scanner';
   public output!: any;
-  public onError(e:any): void {
+  public onError(e: any): void {
     alert(e);
   }
 
-  
+  ngOnInit() {}
 }
