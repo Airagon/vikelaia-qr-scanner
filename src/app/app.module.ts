84535a35069b2508effcf6710521f939927e388b
Airagon <aldo7liverpool@gmail.com>
2022-06-01 12:32:37 +0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgxScannerQrcodeModule } from 'ngx-scanner-qrcode';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgxScannerQrcodeModule],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
